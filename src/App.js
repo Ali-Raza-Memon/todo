434833129725d612196607fbe7ba2375621da227
Ali Raza <123983553+Ali-Raza-Memon@users.noreply.github.com>
2023-11-06 22:03:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route, Link} from 'react-router-dom';
+
+
+function App() {
+
+  return (
+      <>
+
+        <Routes>
+          <Route path='/' element = { } />
+          <Route path='/about-me' element = { } />
+        </Routes>
+
+
+      </>
+
+
+  );
+}
+
+export default App;
